717096348ac1e024d5bfd587247518a22a720b24
Wei-Tse Sun <wtse.sun@gmail.com>
2021-09-21 21:33:30 -0700

    style add semi

--- src/database/index.js
+++ src/database/index.js
@@ -1,23 +1,23 @@
-import cls from 'cls-hooked'
-import { Sequelize } from 'sequelize'
-import { registerModels } from '../models'
+import cls from 'cls-hooked';
+import { Sequelize } from 'sequelize';
+import { registerModels } from '../models';
 
 export default class Database {
   constructor(environment, dbConfig) {
-    this.environment = environment
-    this.dbConfig = dbConfig
-    this.isTestEnvironment = this.environment === 'test'
+    this.environment = environment;
+    this.dbConfig = dbConfig;
+    this.isTestEnvironment = this.environment === 'test';
   }
 
   async connect() {
     // Set up the namespace for transactions -- cls-hooked
-    const namespace = cls.createNamespace('transactions-namespace')
-    Sequelize.useCLS(namespace)
+    const namespace = cls.createNamespace('transactions-namespace');
+    Sequelize.useCLS(namespace);
 
     // Create the connection
     const { username, password, host, port, database, dialect } = this.dbConfig(
       this.environment
-    )
+    );
     this.connection = new Sequelize({
       username,
       password,
@@ -26,21 +26,21 @@ export default class Database {
       database,
       dialect,
       logging: this.isTestEnvironment ? false : console.log,
-    })
+    });
 
-    await this.connection.authenticate({ logging: false })
+    await this.connection.authenticate({ logging: false });
 
     if (!this.isTestEnvironment) {
       console.log(
         'Connection to the database has been established successfully'
-      )
+      );
     }
 
     // Register the models
-    registerModels(this.connection)
+    registerModels(this.connection);
 
     // Sync the models
-    await this.sync()
+    await this.sync();
   }
 
   async disconnect() {}
@@ -49,10 +49,10 @@ export default class Database {
     await this.connection.sync({
       logging: false,
       force: this.isTestEnvironment,
-    })
+    });
 
     if (!this.isTestEnvironment) {
-      console.log('Connection synced successfully')
+      console.log('Connection synced successfully');
     }
   }
 }
--- src/utils/jwt-utils.js
+++ src/utils/jwt-utils.js
@@ -1,21 +1,21 @@
-import jwt from 'jsonwebtoken'
-import environment from '../config/environment'
+import jwt from 'jsonwebtoken';
+import environment from '../config/environment';
 
 export default class JWTUtils {
   static generateAccessToken(payload, options = {}) {
-    const { expiresIn = '1d' } = options
-    return jwt.sign(payload, environment.jwtAccessTokenSecret, { expiresIn })
+    const { expiresIn = '1d' } = options;
+    return jwt.sign(payload, environment.jwtAccessTokenSecret, { expiresIn });
   }
 
   static generateRefreshToken(payload) {
-    return jwt.sign(payload, environment.jwtRefreshTokenSecret)
+    return jwt.sign(payload, environment.jwtRefreshTokenSecret);
   }
 
   static verifyAccessToken(accessToken) {
-    return jwt.verify(accessToken, environment.jwtAccessTokenSecret)
+    return jwt.verify(accessToken, environment.jwtAccessTokenSecret);
   }
 
   static verifyRefreshToken(accessToken) {
-    return jwt.verify(accessToken, environment.jwtRefreshTokenSecret)
+    return jwt.verify(accessToken, environment.jwtRefreshTokenSecret);
   }
 }
